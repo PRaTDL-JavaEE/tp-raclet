5caf8eda4f493ff3c94736e8a86b8fae593ce68c
raclet <raclet@irit.fr>
2021-03-15 08:29:11 +0100

    Resolved #9 : Create

--- src/main/java/friendsofmine/controllers/UtilisateurController.java
+++ src/main/java/friendsofmine/controllers/UtilisateurController.java
@@ -5,8 +5,12 @@ import friendsofmine.services.UtilisateurActiviteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UtilisateurController {
@@ -29,6 +33,22 @@ public class UtilisateurController {
         }
         model.addAttribute("utilisateur", util);
         return "utilisateurShow";
+    }
+
+    @GetMapping("/utilisateur/new")
+    public String createUtilisateur(Model model){
+        model.addAttribute("utilisateur", new Utilisateur());
+        return "utilisateurForm";
+    }
+
+    @PostMapping(value = "/utilisateur")
+    public String createOrUpdateUtilisateur(@Valid Utilisateur util,
+                                            BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "utilisateurForm";
+        }
+        util = utilisateurActiviteService.save(util);
+        return "redirect:/utilisateur/" + util.getId();
     }
 
 }
--- src/test/java/friendsofmine/UtilisateurControllerTest.java
+++ src/test/java/friendsofmine/UtilisateurControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -25,11 +26,12 @@ public class UtilisateurControllerTest {
     @Autowired
     private UtilisateurActiviteService utilisateurActiviteService;
 
-    private Utilisateur util;
+    private Utilisateur util, utilNonSauve;
 
     @BeforeEach
     public void setup() {
         util = utilisateurActiviteService.save(new Utilisateur("Doe", "John", "john@doe.com", "M"));
+        utilNonSauve = new Utilisateur("Morrissey", "Steven Patrick", "momo@rrissey.co.uk", "M");
     }
 
     @Test
@@ -73,5 +75,93 @@ public class UtilisateurControllerTest {
                 .andExpect(view().name("error"))
                 .andDo(print());
     }
+
+    @Test
+    public void testCreateUtilisateur() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges Mvc
+        // when: on simule du requête HTTP de type GET vers "/utilisateur/new"
+        // then: la requête est acceptée (status OK) et la vue "utilisateurForm" est rendue
+        mockMvc.perform(get("/utilisateur/new"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("utilisateurForm"))
+                .andDo(print());
+    }
+
+    @Test
+    public void testCreateUtilisateurValide() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges Mvc
+        // when: on simule du requête HTTP de type POST vers "/utilisateur/"
+        // when: la requête comporte un Utilisateur valide
+        // then: le status de la reqûete est FOUND
+        // then: une redirection a lieu vers l'adresse /utilisateur/{id} où id est l'id de l'utilisateur créé
+        mockMvc.perform(post("/utilisateur")
+                .param("nom", utilNonSauve.getNom())
+                .param("prenom", utilNonSauve.getPrenom())
+                .param("email", utilNonSauve.getEmail())
+                .param("sexe", utilNonSauve.getSexe()))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrlPattern("/utilisateur/*"))
+                .andDo(print());
+    }
+
+    @Test
+    public void testCreateUtilisateurNomInvalide() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges Mvc
+        // when: on simule du requête HTTP de type POST vers "/utilisateur/"
+        // when: la requête comporte un Utilisateur invalide
+        // then: la requête est acceptée (status OK)
+        // then: la vue "utilisateurForm" est rendue avec un message d'erreur de validation
+        mockMvc.perform(post("/utilisateur")
+                .param("nom", "")
+                .param("prenom", utilNonSauve.getPrenom())
+                .param("email", utilNonSauve.getEmail())
+                .param("sexe", utilNonSauve.getSexe()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("utilisateurForm"))
+                .andExpect(content().string(Matchers.containsString("size must be between 1 and")))
+                .andDo(print());
+    }
+
+    @Test
+    public void testCreateUtilisateurPrenomInvalide() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges Mvc
+        // when: on simule du requête HTTP de type POST vers "/utilisateur/"
+        // when: la requête comporte un Utilisateur invalide
+        // then: la requête est acceptée (status OK)
+        // then: la vue "utilisateurForm" est rendue avec un message d'erreur de validation
+        mockMvc.perform(post("/utilisateur")
+                .param("nom", utilNonSauve.getNom())
+                .param("prenom", "")
+                .param("email", utilNonSauve.getEmail())
+                .param("sexe", utilNonSauve.getSexe()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("utilisateurForm"))
+                .andExpect(content().string(Matchers.containsString("size must be between 1 and")))
+                .andDo(print());
+    }
+
+    @Test
+    public void testCreateUtilisateurEmailInvalide() throws Exception{
+        // given: un objet MockMvc qui simulate des échanges Mvc
+        // when: on simule du requête HTTP de type POST vers "/utilisateur/"
+        // when: la requête comporte un Utilisateur invalide
+        // then: la requête est acceptée (status OK)
+        // then: la vue "utilisateurForm" est rendue avec un message d'erreur de validation
+        mockMvc.perform(post("/utilisateur")
+                .param("nom", utilNonSauve.getNom())
+                .param("prenom", utilNonSauve.getPrenom())
+                .param("email", "toto")
+                .param("sexe", utilNonSauve.getSexe()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("utilisateurForm"))
+                .andExpect(content().string(Matchers.containsString("must be a well-formed email address")))
+                .andDo(print());
+    }
+
+
 
 }
